5be4791f363e3439038fc9336714a4c2b45522fe
Clyde Lu <92775935+p42777@users.noreply.github.com>
2021-11-23 03:07:08 +0800

    Update 4.lcm.c

--- W4/4.lcm.c
+++ W4/4.lcm.c
@@ -28,9 +28,9 @@ int main(void){
     scanf("%d%d%d", &x, &y, &z);
     scanf("%d%d%d", &a, &b, &c);
 
-    for (i=1; i<=x*y*z; i++) {
+    for (i = 1 ; i <= x * y * z ; i++) {
         
-          if (i%x==a && i%y==b && i%z==c) {
+          if (i % x == a && i % y == b && i % z == c) {
           printf("%d\n", i);
 
           break;
