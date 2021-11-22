f827befa499f3312c68369e28298d7a5c3c05469
Clyde Lu <92775935+p42777@users.noreply.github.com>
2021-11-23 03:12:58 +0800

    Add files via upload

---
+++ W1-2/1.helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  // include information about the standard input/output library
+#include <stdlib.h> 
+
+int main(void) {
+	/*
+	return type : int
+	arguments : void(null)
+	main : Entry Point
+	*/
+	printf("Hello World!\n"); // \n : 換行符號
+	return 0; 
+	// return an integer : 0
+}
